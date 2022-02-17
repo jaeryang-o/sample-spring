b7361d646a397a8cf1280cd67c148079a6db65ae
ohjaeryang <ohjaeryang@gamil.com>
2022-02-17 16:06:54 +0900

    'done'

---
+++ SampleSpringYse/src/main/java/sample/spring/yse/BookServiceImplTest.java
@@ -0,0 +1,14 @@
+package sample.spring.yse;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class BookServiceImplTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
